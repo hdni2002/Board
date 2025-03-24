74c8c0b403b32fd6b29df580531f3ca582d44987
hdni2002 <hdni2003@gmail.com>
2025-03-24 21:22:31 +0900

    Initial commit

---
+++ src/main/java/com/example/board/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller 
+public class HomeController {
+	
+	@GetMapping("/") // 현재경로로 접근 
+	public String index() {
+		return "index"; // 웹페이지
+	}
+	public String a() {
+		return "";
+	}
+}
